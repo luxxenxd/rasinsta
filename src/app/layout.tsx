5542acebee0b27d5904d651ed35e6ca52ce2889e
luxxenxd <luxxenos123@gmail.com>
2024-11-27 14:37:59 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "../components/NavBar";
+
 
 export const metadata: Metadata = {
   title: "SnapZoška",
@@ -14,7 +16,12 @@ export default function RootLayout({
   return (
     <html lang="sk">
       <body>
-        {children}
+        <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+          <main style={{ flexGrow: 1 }}>
+            {children}
+          </main>
+          <NavBar /> 
+        </div>
       </body>
     </html>
   );
