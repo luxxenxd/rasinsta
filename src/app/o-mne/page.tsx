0f41435cd563c7e039c85b1b9b5598c39defa857
luxxenxd <luxxenos123@gmail.com>
2024-12-11 14:10:21 +0100

    omne

--- src/app/o-mne/page.tsx
+++ src/app/o-mne/page.tsx
@@ -1,10 +1,10 @@
-// src/app/o-nas/page.tsx
+// src/app/o-mne/page.tsx
 
 
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-export const metadata = { title: "O nás | ZoškaSnap" };
+export const metadata = { title: "O mne | ZoškaSnap" };
 
 export default function AboutUs() {
 
